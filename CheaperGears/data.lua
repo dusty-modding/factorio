e7285e1d9fc1c061ca0bc0abf0c9c497fd675266
dhershman1 <dustinh17@gmail.com>
2024-10-25 19:51:30 -0400

    Added: CheaperGears Mod

---
+++ CheaperGears/data.lua
@@ -0,0 +1,8 @@
+
+--recipe
+require("prototypes.recipe.new-recipes")
+
+
+
+
+
---
+++ CheaperGears/prototypes/recipe/new-recipes.lua
@@ -0,0 +1,9 @@
+data:extend(
+{
+  {
+    type = "recipe",
+    name = "iron-gear-wheel",
+    ingredients = {{type="item", name="iron-plate", amount=1}},
+    results = {{type="item", name="iron-gear-wheel", amount=1}}
+  }
+})
