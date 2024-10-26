10315b485b5aa96d902bfb554bac34784038ac06
dhershman1 <dustinh17@gmail.com>
2024-10-26 14:49:57 -0400

    Added: CheapRailsAndConcrete Mod

---
+++ CheapRailsAndConcrete/data-updates.lua
@@ -0,0 +1,2 @@
+data.raw["recipe"]["rail"].results[1].amount = data.raw["recipe"]["rail"].results[1].amount * 5
+data.raw["recipe"]["concrete"].results[1].amount = data.raw["recipe"]["concrete"].results[1].amount * 10
