4e06bc6911feab5329dce43f34a35e7bb9dba7fb
dhershman1 <dustinh17@gmail.com>
2024-10-26 15:23:09 -0400

    Updated: Added config to cheap rails and concrete

--- CheapRailsAndConcrete/data-updates.lua
+++ CheapRailsAndConcrete/data-updates.lua
@@ -1,2 +1,3 @@
-data.raw["recipe"]["rail"].results[1].amount = data.raw["recipe"]["rail"].results[1].amount * 5
-data.raw["recipe"]["concrete"].results[1].amount = data.raw["recipe"]["concrete"].results[1].amount * 10
+data.raw["recipe"]["rail"].results[1].amount = data.raw["recipe"]["rail"].results[1].amount * settings.startup["multiply_rails_by"].value
+data.raw["recipe"]["concrete"].results[1].amount = data.raw["recipe"]["concrete"].results[1].amount * settings.startup["multiply_concrete_by"].value
+data.raw["recipe"]["refined-concrete"].results[1].amount = data.raw["recipe"]["refined-concrete"].results[1].amount * settings.startup["multiply_refined_concrete"].value
---
+++ CheapRailsAndConcrete/settings.lua
@@ -0,0 +1,29 @@
+data:extend({
+  {
+    type = 'int-setting',
+    name = 'multiply_rails_by',
+    setting_type = 'startup',
+    default_value = 5,
+    minimum_value = 1,
+    maximum_value = 100,
+    order = 'a'
+  },
+  {
+    type = 'int-setting',
+    name = 'multiply_concrete_by',
+    setting_type = 'startup',
+    default_value = 5,
+    minimum_value = 1,
+    maximum_value = 100,
+    order = 'b'
+  },
+  {
+    type = 'int-setting',
+    name = 'multiply_refined_concrete',
+    setting_type = 'startup',
+    default_value = 5,
+    minimum_value = 1,
+    maximum_value = 100,
+    order = 'c'
+  }
+})
